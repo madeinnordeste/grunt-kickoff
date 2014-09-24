c8822b7a58fff38b119dfe1726ca49961c227110
Luiz Alberto <madeinnordeste@gmail.com>
2014-09-24 12:19:35 -0300

    modificar package e add Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function( grunt ) {
+ 
+  grunt.initConfig({
+
+    uglify : {
+      options : {
+        mangle : false
+      },
+
+      my_target : {
+        files : {
+          'assets/js/main.js' : [ 'assets/js/one.js', 'assets/js/two.js' ]
+        }
+      }
+    }, // uglify
+
+
+    watch : {
+      dist : {
+        files : [
+          'assets/js/**/*',
+          'assets/sass/**/*'
+        ],
+
+        tasks : [ 'uglify' ]
+      }
+    } // watch
+
+  });
+
+
+  // Plugins do Grunt
+  grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+  grunt.loadNpmTasks( 'grunt-contrib-watch' );
+
+  
+  // Tarefas que serão executadas
+  grunt.registerTask( 'default', [ 'uglify'] );
+
+   // Tarefa para Watch
+  grunt.registerTask( 'w', [ 'watch' ] );
+
+};
--- README.md
+++ README.md
@@ -2,3 +2,40 @@ grunt-kickoff
 =============
 
 grunt boilerplate files
+
+
+## Iniciando
+
+- Instalar o Node [Node.JS](http://nodejs.org/).
+
+- Clonar
+
+```bash
+$ git clone https://github.com/madeinnordeste/grunt-kickoff.git
+```
+
+- Entrar no diretório
+
+```bash
+$ cd grunt-kickoff
+```
+
+- Instalar as dependencias
+
+```bash
+$ sudo npm install
+```
+
+- Modificar `package.json`, `Gruntfile.js` com as informações do seu projeto
+
+
+Customnizar as tarefas de acordo com sua necessidade.for your needs
+
+- Executar o Grunt 
+
+```bash
+$ sudo grunt
+```
+
+
+
